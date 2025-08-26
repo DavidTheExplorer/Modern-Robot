91662ce93b44333f8dc3db2397e2f658555aa15b
David <darknight4123@gmail.com>
2025-08-27 02:30:11 +0300

    feat: added a way to control the typing speed of Keyboard

--- src/main/java/dte/modernrobot/keyboard/Keyboard.java
+++ src/main/java/dte/modernrobot/keyboard/Keyboard.java
@@ -4,10 +4,24 @@ public interface Keyboard
 {
     /**
      * Types all the characters in the provided {@code text}, in their appearance order.
+     * <p>
+     * This method works at maximum speed, so it's not meant to mimic a human.
+     * To control the typing speed, use {@link #type(String, TypingSpeed)}.
      *
      * @param text The text.
      */
-    void type(String text);
+    default void type(String text)
+    {
+        type(text, TypingSpeed.FASTEST);
+    }
+
+    /**
+     * Types all the characters in the provided {@code text}, in their appearance order, at the provided {@code speed}.
+     *
+     * @param text The text.
+     * @param speed The speed.
+     */
+    void type(String text, TypingSpeed speed);
 
     /**
      * Presses all the function keys in the provided {@code array}, in their appearance order.
